2f99b00e5e20d1f275eb9baeace6ce1196366f57
neemkashu <sparjinkust@mail.ru>
2023-07-06 22:12:01 +0300

    fix: undo skip

--- test/hermione/cart.hermione.ts
+++ test/hermione/cart.hermione.ts
@@ -3,19 +3,19 @@ import { BASE_URL } from './constants';
 import { addBug, writeLog } from '../helpers';
 
 hermione.skip.in('chromeTablet', 'проверяем только на десктопе');
-describe.only('Корзина в каталоге', async function () {
+describe('Корзина в каталоге', async function () {
 
     it('сохраняются данные после перезугразки', async function () {
         const productPath = addBug(BASE_URL + ROUTES_STATIC.catalog + '/0');
 
         await this.browser.url(productPath);
         await this.browser.pause(1000)
-        
+
         const button = await this.browser.$$('button')[1];
         await this.browser.pause(1000)
-        
+
         await button.click();
-        
+
         await this.browser.refresh();
         await this.browser.pause(200);
         const puppeteer = this.browser.puppeteer;
